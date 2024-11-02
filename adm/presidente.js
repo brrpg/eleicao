0a39a4746711682d7ed514ff095827b1d0b7ad81
brrpg <brasilrpghb@gmail.com>
2024-11-02 16:58:35 -0300

    Create presidente.js

---
+++ adm/presidente.js
@@ -0,0 +1,197 @@
+//let resultados = [];  // Variável para armazenar os resultados calculados
+
+// Função para gerar os campos de candidatos
+function gerarCampos() {
+    const numeroCandidatos = parseInt(document.getElementById("numeroCandidatos").value);
+    const container = document.getElementById("partidos-container");
+
+    // Limpa o container antes de gerar novos campos
+    container.innerHTML = "";
+
+    for (let i = 1; i <= numeroCandidatos; i++) {
+        const candidatoDiv = document.createElement("div");
+        candidatoDiv.classList.add("candidato", "form-group", "row", "border", "p-3", "mb-2");
+    
+        const candidatoTitulo = document.createElement("h5");
+        candidatoTitulo.innerText = `Candidato ${i}:`;
+        candidatoTitulo.classList.add("col-12");
+        candidatoDiv.appendChild(candidatoTitulo);
+    
+        const nomeInput = document.createElement("input");
+        nomeInput.type = "text";
+        nomeInput.placeholder = "Nome do Candidato";
+        nomeInput.classList.add("form-control", "mb-2");
+        nomeInput.id = `candidatoNome${i}`;
+        candidatoDiv.appendChild(nomeInput);
+    
+        // Div para os inputs do partido, votos, cor e imagem
+        const camposDiv = document.createElement("div");
+        camposDiv.classList.add("row", "col-12", "pe-0"); // Define a linha que conterá os outros campos
+        candidatoDiv.appendChild(camposDiv);
+    
+        // Campo Partido
+        const partidoDiv = document.createElement("div");
+        partidoDiv.classList.add("col-lg-4", "col-3", "mx-1", "mb-2", "p-0");
+        camposDiv.appendChild(partidoDiv);
+        
+        const partidoInput = document.createElement("input");
+        partidoInput.type = "text";
+        partidoInput.placeholder = "Partido";
+        partidoInput.classList.add("form-control");
+        partidoInput.id = `candidatoPartido${i}`;
+        partidoDiv.appendChild(partidoInput);
+    
+        // Campo Votos
+        const votosDiv = document.createElement("div");
+        votosDiv.classList.add("col-lg-4", "col-3", "ms-1", "mb-2", "p-0");
+        camposDiv.appendChild(votosDiv);
+        
+        const votosInput = document.createElement("input");
+        votosInput.type = "number";
+        votosInput.placeholder = "Votos";
+        votosInput.classList.add("form-control");
+        votosInput.id = `candidatoVotos${i}`;
+        votosDiv.appendChild(votosInput);
+
+        // Campo Voto NPC
+        const votosNPCDiv = document.createElement("div");
+        votosNPCDiv.classList.add("col-lg-3", "col-3", "mx-1", "mb-2", "p-0");
+        camposDiv.appendChild(votosNPCDiv);
+        
+        const votosNPCInput = document.createElement("input");
+        votosNPCInput.type = "number";
+        votosNPCInput.placeholder = "% NPC";
+        votosNPCInput.classList.add("form-control");
+        votosNPCInput.id = `candidatoNpc${i}`;
+        votosNPCDiv.appendChild(votosNPCInput);
+    
+        // Campo Cor
+        const corDiv = document.createElement("div");
+        corDiv.classList.add("col-lg-2", "col-1", "mb-2", "p-0");
+        camposDiv.appendChild(corDiv);
+        
+        const corInput = document.createElement("input");
+        corInput.type = "color";
+        corInput.classList.add("form-control", "form-control-color");
+        corInput.id = `candidatoCor${i}`;
+        corDiv.appendChild(corInput);
+    
+        // Campo Imagem (URL)
+        const imagemDiv = document.createElement("div");
+        imagemDiv.classList.add("col-lg-10", "col-12", "mb-2", "p-0");
+        camposDiv.appendChild(imagemDiv);
+        
+        const imagemInput = document.createElement("input");
+        imagemInput.type = "text";
+        imagemInput.placeholder = "URL da Imagem do Candidato";
+        imagemInput.classList.add("form-control");
+        imagemInput.id = `candidatoImagem${i}`;
+        imagemDiv.appendChild(imagemInput);
+    
+        container.appendChild(candidatoDiv);
+    }
+    
+}
+
+function calcularDistribuicao() {
+    const nulosBrancos = parseInt(document.getElementById("nulosBrancos").value) || 0;
+    const numeroCandidatos = parseInt(document.getElementById("numeroCandidatos").value);
+
+    const candidatos = [];
+    for (let i = 1; i <= numeroCandidatos; i++) {
+        const nome = document.getElementById(`candidatoNome${i}`).value;
+        const votos = parseInt(document.getElementById(`candidatoVotos${i}`).value) || 0;
+        const partido = document.getElementById(`candidatoPartido${i}`).value;
+        const cor = document.getElementById(`candidatoCor${i}`).value;
+        const imagem = document.getElementById(`candidatoImagem${i}`).value;
+        const npc = document.getElementById(`candidatoNpc${i}`).value;
+
+        candidatos.push({ nome, votos, partido, cor, imagem, npc });
+    }
+
+    const totalVotos = nulosBrancos + candidatos.reduce((acc, cand) => acc + cand.votos, 0);
+    const votosValidos = totalVotos - nulosBrancos;
+    resultados = candidatos.map(cand => ({
+        ...cand,
+        porcentagem: votosValidos > 0 ? ((cand.votos / votosValidos) * 100).toFixed(2) : 0
+    }));
+
+    let resultadoHTML = `<p class="mb-0"><strong>Votos válidos:</strong> ${votosValidos}</p>`;
+    resultadoHTML += `<p class="mb-0"><strong>Votos nulos/brancos:</strong> ${nulosBrancos}</p>`;
+    resultadoHTML += `<p class="mb-3"><strong>Total de votos:</strong> ${totalVotos}</p>`;
+
+    resultados.forEach(cand => {
+        resultadoHTML += `<p><strong>${cand.nome}</strong> (${cand.partido}) - Votos: ${cand.votos} (${cand.porcentagem}%) - NPC: ${cand.npc}%</p>`;
+    });
+
+    document.getElementById("resultado").innerHTML = resultadoHTML;
+}
+
+function enviarParaPlanilha() {
+    const nulosBrancos = parseInt(document.getElementById("nulosBrancos").value) || 0;
+    const totalVotos = nulosBrancos + resultados.reduce((acc, cand) => acc + parseInt(cand.votos), 0);
+    const votosValidos = totalVotos - nulosBrancos;
+
+    const urlPrincipal = "https://api.steinhq.com/v1/storages/67265bf0c0883333654a1cd7/Presidente";
+    const dadosParaEnviar = resultados.map(cand => ({
+        PresidenteVotosNulosBrancos: nulosBrancos,
+        PresidenteVotosValidos: votosValidos,
+        PresidenteTotalVotos: totalVotos,
+        PresidenteCandidato: cand.nome,
+        PresidentePartido: cand.partido,
+        PresidenteVotos: cand.votos,
+        PresidentePorcentagem: cand.porcentagem,
+        PresidenteImagem: cand.imagem,
+        PresidenteNPC: cand.npc,
+    }));
+
+    // Enviando para a primeira planilha
+    fetch(urlPrincipal, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosParaEnviar)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Dados enviados para o Stein com sucesso:", data);
+        // Enviando para a segunda planilha com informações selecionadas
+        const urlSecundaria = "https://api.steinhq.com/v1/storages/67267f05c0883333654a2351/Presidente";
+        const dadosParaEnviarSecundaria = resultados.map(cand => ({
+            Candidato: cand.nome,
+            Partido: cand.partido,
+            Imagem: cand.imagem,
+            CandidatoCor: cand.cor,
+        }));
+
+        return fetch(urlSecundaria, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dadosParaEnviarSecundaria)
+        });
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Dados enviados para a segunda planilha com sucesso:", data);
+        Swal.fire({
+            title: "Enviado!",
+            text: "Dados enviados para ambas as planilhas",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 2500
+        });
+    })
+    .catch(error => {
+        console.error("Erro ao enviar dados:", error);
+        Swal.fire({
+            title: "Erro!",
+            text: "Erro ao enviar dados. Verifique o console para mais detalhes.",
+            icon: "error",
+            showConfirmButton: false,
+            timer: 2500
+        });
+    });
+}
